0760d6b075735bda42609ebda09d333a30fabe1e
Simon Lynch <simon.lynch@hashicorp.com>
2024-09-06 11:28:20 +1000

    chore: Update Kubernetes provider configuration

--- providers.tf
+++ providers.tf
@@ -2,6 +2,9 @@
 provider "kubernetes" {
   # Configuration options
   host = var.k8s_api_server
+  cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
+  client_key = base64decode(var.client_key)
+  client_certificate = base64decode(var.client_certificate)
   
 }
 
@@ -9,6 +12,8 @@ provider "helm" {
   # Configuration options
   kubernetes {  
     host = var.k8s_api_server
-  
+    cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
+    client_key = base64decode(var.client_key)
+    client_certificate = base64decode(var.client_certificate)
   }
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,19 @@ variable "k8s_api_server" {
     type = string
     description = "The hostname (in form of URI) of the Kubernetes API"
 }
+
+variable "client_certificate" {
+    type = string
+    description = "The client certificate for authenticating to the Kubernetes cluster"
+}
+
+variable "client_key" {
+    type = string
+    description = "The client key for authenticating to the Kubernetes cluster"
+    sensitive = true
+}
+
+variable "cluster_ca_certificate" {
+    type = string
+    description = "The CA certificate for authenticating to the Kubernetes cluster"
+}
