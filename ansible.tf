24bb25d8ae8fa1b44944fc8565d4d80e62784d04
Simon Lynch <simon.lynch@hashicorp.com>
2024-09-09 15:17:07 +1000

    chore: Update namespace and source for ansible-automation-platform-operator

--- ansible.tf
+++ ansible.tf
@@ -1,16 +1,16 @@
-# resource "kubernetes_namespace" "aap" {
-#   metadata {
-#     name = "ansible"
-#   }
-# }
+resource "kubernetes_namespace" "aap" {
+  metadata {
+    name = "aap"
+  }
+}
 
 
-# resource "kubernetes_manifest" "aap-operatorgroup" {
-# depends_on = [ kubernetes_namespace.aap ]
-#   manifest = provider::kubernetes::manifest_decode(local.aap_operatorgroup)
-# }
+resource "kubernetes_manifest" "aap-operatorgroup" {
+depends_on = [ kubernetes_namespace.aap ]
+  manifest = provider::kubernetes::manifest_decode(local.aap_operatorgroup)
+}
 
-# resource "kubernetes_manifest" "aap-subscription" {
-# depends_on = [ kubernetes_namespace.aap ]
-#   manifest = provider::kubernetes::manifest_decode(local.aap_subscription)
-# }
+resource "kubernetes_manifest" "aap-subscription" {
+depends_on = [ kubernetes_namespace.aap ]
+  manifest = provider::kubernetes::manifest_decode(local.aap_subscription)
+}
