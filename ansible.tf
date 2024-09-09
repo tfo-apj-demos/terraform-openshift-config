b925fe90723cc2e859b0379fc98f7ae1bde000b3
Simon Lynch <simon.lynch@hashicorp.com>
2024-09-09 15:04:42 +1000

    chore: Remove commented out code for aap-operatorgroup and aap-subscription

--- ansible.tf
+++ ansible.tf
@@ -5,12 +5,12 @@ resource "kubernetes_namespace" "aap" {
 }
 
 
-resource "kubernetes_manifest" "aap-operatorgroup" {
-depends_on = [ kubernetes_namespace.aap ]
-  manifest = provider::kubernetes::manifest_decode(local.aap_operatorgroup)
-}
+# resource "kubernetes_manifest" "aap-operatorgroup" {
+# depends_on = [ kubernetes_namespace.aap ]
+#   manifest = provider::kubernetes::manifest_decode(local.aap_operatorgroup)
+# }
 
-resource "kubernetes_manifest" "aap-subscription" {
-depends_on = [ kubernetes_namespace.aap ]
-  manifest = provider::kubernetes::manifest_decode(local.aap_subscription)
-}
+# resource "kubernetes_manifest" "aap-subscription" {
+# depends_on = [ kubernetes_namespace.aap ]
+#   manifest = provider::kubernetes::manifest_decode(local.aap_subscription)
+# }
