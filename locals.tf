4d85fe18bba6f1930f6d9c8b0bb0b8aede084842
Simon Lynch <simon.lynch@hashicorp.com>
2024-09-11 06:09:54 +1000

    chore: Update aap-eda.yaml to include database section

--- locals.tf
+++ locals.tf
@@ -1,13 +1,13 @@
 locals {
-pg_subscription = file("${path.module}/manifests/postgres-subscription.yaml")
-pg_cluster = file("${path.module}/manifests/postgres-cluster.yaml")
-redis_operatorgroup = file("${path.module}/manifests/redis-operatorgroup.yaml")
-redis_subscription = file("${path.module}/manifests/redis-subscription.yaml")
-redis_cluster = file("${path.module}/manifests/redis-cluster.yaml")
+pg_subscription = file("${path.module}/manifests/tfe/postgres-subscription.yaml")
+pg_cluster = file("${path.module}/manifests/tfe/postgres-cluster.yaml")
+redis_operatorgroup = file("${path.module}/manifests/tfe/redis-operatorgroup.yaml")
+redis_subscription = file("${path.module}/manifests/tde/redis-subscription.yaml")
+redis_cluster = file("${path.module}/manifests/tfe/redis-cluster.yaml")
 
 #ansible
-aap_subscription = file("${path.module}/manifests/ansible-app-subscription.yaml")
-aap_operatorgroup = file("${path.module}/manifests/ansible-operatorgroup.yaml")
-aap_automationcontroller = file("${path.module}/manifests/aap-automationcontroller.yaml")
-aap_eda = file("${path.module}/manifests/aap-eda.yaml")
+aap_subscription = file("${path.module}/manifests/ansible/ansible-app-subscription.yaml")
+aap_operatorgroup = file("${path.module}/manifests/ansible/ansible-operatorgroup.yaml")
+aap_automationcontroller = file("${path.module}/manifests/ansible/aap-automationcontroller.yaml")
+aap_eda = file("${path.module}/manifests/ansible/aap-eda.yaml")
 }
