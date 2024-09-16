1787617dde23ce9fce011ca55a74522df0cf8db1
Simon Lynch <simon.lynch@hashicorp.com>
2024-09-16 22:45:32 +1000

    Refactor Vault Operator subscription and remove unnecessary code

--- vault.tf
+++ vault.tf
@@ -1,8 +1,3 @@
-resource "kubernetes_namespace" "vault" {
-  metadata {
-    name = "vault"
-  }
-}
 
 resource "kubernetes_manifest" "vault-operator" {
   depends_on = [ kubernetes_namespace.vault ]
