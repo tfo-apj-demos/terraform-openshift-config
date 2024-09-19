bfcf4bb16e2214a09d2d06625d01776c6b812787
Simon Lynch <simon.lynch@hashicorp.com>
2024-09-19 16:55:45 +1000

    Refactor outputs.tf to use manifest_encode for tfe_helm_output value

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "tfe_helm_output" {
-  value = nonsensitive( provider::kubernetes::manifest_decode( yamlencode(local.tfe_helm_values) ) )
+  value = nonsensitive( provider::kubernetes::manifest_encode(local.tfe_helm_values) )
   sensitive = false
 }
