bb04eb576f7b57710ec9884d02a2ef19d3b01690
Simon Lynch <simon.lynch@hashicorp.com>
2024-09-20 09:41:46 +1000

    Refactor Kubernetes namespace configurations to ignore changes in certain annotations and labels

--- ansible.tf
+++ ansible.tf
@@ -7,7 +7,8 @@ resource "kubernetes_namespace" "aap" {
     ignore_changes = [
         metadata.0.annotations["openshift.io/sa.scc.mcs"],
         metadata.0.annotations["openshift.io/sa.scc.supplemental-groups"],
-        metadata.0.annotations["openshift.io/sa.scc.uid-range"]
+        metadata.0.annotations["openshift.io/sa.scc.uid-range"],
+        metadata.0.labels["olm.operatorgroup.uid/4a8b45e7-3b75-435a-9247-439bcfb63c1f"]
     ]
   }
   
