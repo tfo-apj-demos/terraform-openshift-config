e7390c38302efe0b928215ce6092f8d0255751a3
Simon Lynch <simon.lynch@hashicorp.com>
2024-09-20 07:18:33 +1000

    Refactor Kubernetes manifest for vault-auth-prereqs in vault.tf

--- vault.tf
+++ vault.tf
@@ -3,7 +3,14 @@ resource "kubernetes_manifest" "vault-operator" {
   manifest = provider::kubernetes::manifest_decode(local.vault_operator)
 }
 
+resource kubernetes_namespace "vault" {
+  metadata {
+    name = "vault"
+  }
+}
+
 
 resource "kubernetes_manifest" "auth_prereqs" {
+  depends_on = [ kubernetes_namespace.vault ]
   manifest = provider::kubernetes::manifest_decode_multi(local.vault_auth_prereqs)
 }
