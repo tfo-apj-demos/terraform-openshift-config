fe40f12dd73eefa2757c61aa56165f462151b6af
Simon Lynch <simon.lynch@hashicorp.com>
2024-09-21 18:52:23 +1000

    Refactor postgres-scc.yaml to update security context constraints

--- tfe.tf
+++ tfe.tf
@@ -35,10 +35,10 @@ resource "kubernetes_manifest" "pg-operator" {
   manifest = provider::kubernetes::manifest_decode(local.pg_subscription)
 }
 
-resource "kubernetes_manifest" "pg-scc" {
-  depends_on = [ kubernetes_namespace.tfe ]
-  manifest = provider::kubernetes::manifest_decode(local.pg_scc)
-}
+# resource "kubernetes_manifest" "pg-scc" {
+#   depends_on = [ kubernetes_namespace.tfe ]
+#   manifest = provider::kubernetes::manifest_decode(local.pg_scc)
+# }
 
 
 
