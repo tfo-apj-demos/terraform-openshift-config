6ca49a7344794ab018fbf7763607ed77dbb67557
Aaron Evans <aarone@outlook.com.au>
2024-09-27 14:54:25 +1000

    Add controller target module and configure services in boundary.tf

--- boundary.tf
+++ boundary.tf
@@ -35,3 +35,23 @@ module "oauth_target" {
     use_vault_creds    = false
   }]
 }
+
+# create another target for controller-aap.apps.openshift-01.hashicorp.local
+module "controller_target" {
+  source = "app.terraform.io/tfo-apj-demos/target/boundary"
+  version = "~> 2.0.1"
+
+  project_name           = "gcve_admins"
+  hostname_prefix        = "Controller Openshift Console"
+
+  hosts = [{
+    fqdn  = "controller-aap.apps.openshift-01.hashicorp.local"
+  }]
+
+  services = [{
+    type             = "tcp"
+    port             = 443
+    use_existing_creds = false
+    use_vault_creds    = false
+  }]
+}
