743b323d65ae35ebfff16a4cc151b742396c37c6
Aaron Evans <aarone@outlook.com.au>
2024-10-22 10:24:40 +1100

    Refactor EDA manifest resource to utilize locals for manifest decoding and streamline resource definitions

--- eda.tf
+++ eda.tf
@@ -1,36 +1,18 @@
-# resource "kubernetes_namespace" "eda" {
-#   metadata {
-#     name = "eda"
-#   }  
-#   lifecycle{
-#     ignore_changes = [
-#         metadata.0.annotations["openshift.io/sa.scc.mcs"],
-#         metadata.0.annotations["openshift.io/sa.scc.supplemental-groups"],
-#         metadata.0.annotations["openshift.io/sa.scc.uid-range"]
-#     ]
-#   }
-# }
+locals {
+  eda_manifests = provider::kubernetes::manifest_decode_multi(local.eda)
+}
 
-# AWX
+resource "kubernetes_manifest" "eda-namespace" {
+  manifest = provider::kubernetes::manifest_decode(local.eda_namespace)
+}
 
+resource "kubernetes_manifest" "eda" {
+  depends_on = [ kubernetes_manifest.eda-namespace ]
 
-# locals {
-#   eda_manifests = provider::kubernetes::manifest_decode_multi(local.eda)
-# }
+  for_each = {
+    for manifest in local.eda_manifests :
+    "${manifest.kind}-${manifest.metadata.name}" => manifest
+  }
 
-# resource "kubernetes_manifest" "eda-namespace" {
-#   manifest = provider::kubernetes::manifest_decode(local.eda_namespace)
-# }
-
-
-
-# resource "kubernetes_manifest" "eda" {
-#   depends_on = [ kubernetes_manifest.eda-namespace ]
-
-#   for_each = {
-#     for manifest in local.eda_manifests :
-#     "${manifest.kind}-${manifest.metadata.name}" => manifest
-#   }
-
-#   manifest = each.value
-# }
+  manifest = each.value
+}
