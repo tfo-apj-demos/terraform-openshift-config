84d5405913e17333189cc1a41cc0aa38a245ac89
Aaron Evans <aarone@outlook.com.au>
2024-10-22 09:28:31 +1100

    Update EDA namespace resource and change manifest decoding method

--- eda.tf
+++ eda.tf
@@ -1,18 +1,18 @@
-resource "kubernetes_namespace" "eda" {
-  metadata {
-    name = "eda"
-  }  
-  lifecycle{
-    ignore_changes = [
-        metadata.0.annotations["openshift.io/sa.scc.mcs"],
-        metadata.0.annotations["openshift.io/sa.scc.supplemental-groups"],
-        metadata.0.annotations["openshift.io/sa.scc.uid-range"]
-    ]
-  }
-}
+# resource "kubernetes_namespace" "eda" {
+#   metadata {
+#     name = "eda"
+#   }  
+#   lifecycle{
+#     ignore_changes = [
+#         metadata.0.annotations["openshift.io/sa.scc.mcs"],
+#         metadata.0.annotations["openshift.io/sa.scc.supplemental-groups"],
+#         metadata.0.annotations["openshift.io/sa.scc.uid-range"]
+#     ]
+#   }
+# }
 
 # AWX
 resource "kubernetes_manifest" "eda" {
     depends_on = [ kubernetes_namespace.eda ]
-  manifest = provider::kubernetes::manifest_decode(local.eda)
+  manifest = provider::kubernetes::manifest_decode_multi(local.eda)
 }
