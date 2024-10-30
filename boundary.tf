aa80c5a3d6d5ab1eb837ef97efba47db8204dec8
Aaron Evans <aarone@outlook.com.au>
2024-10-30 20:46:17 +1100

    Refactor boundary.tf to consolidate OpenShift targets and improve clarity

--- boundary.tf
+++ boundary.tf
@@ -1,81 +1,33 @@
-/*module "openshift_target" {
-  source  = "app.terraform.io/tfo-apj-demos/target/boundary"
-  version = "~> 2.0.1"
+# OpenShift Console Target
+module "openshift_console_target" {
+  source               = "github.com/tfo-apj-demos/terraform-boundary-target-refactored"
 
-  project_name           = "gcve_admins"
-  hostname_prefix        = "On-Prem Openshift Console"
+  project_name         = "gcve_admins"
+  target_name          = "OpenShift"
+  hosts                = ["console-openshift-console.apps.openshift-01.hashicorp.local"]
+  port                 = 443
+  target_type          = "tcp"
 
-  hosts = [{
-    fqdn  = "console-openshift-console.apps.openshift-01.hashicorp.local"
-  }]
+  # Vault credential configurations
+  use_credentials      = false
 
-  services = [{
-    type             = "tcp"
-    port             = 443
-    use_existing_creds = false
-    use_vault_creds    = false
-  }]
+  # Alias name for accessing the AAP Openshift Console
+  alias_name           = "console-openshift-console.apps.openshift-01.hashicorp.local"
 }
 
-module "oauth_target" {
-  source  = "app.terraform.io/tfo-apj-demos/target/boundary"
-  version = "~> 2.0.1"
+# Oauth Target for Openshift Console
+module "openshift_console_target" {
+  source               = "github.com/tfo-apj-demos/terraform-boundary-target-refactored"
 
-  project_name           = "gcve_admins"
-  hostname_prefix        = "OAuth Openshift Console"
+  project_name         = "gcve_admins"
+  target_name          = "OAuth for OpenShift"
+  hosts                = ["oauth-openshift.apps.openshift-01.hashicorp.local"]
+  port                 = 443
+  target_type          = "tcp"
 
-  hosts = [{
-    fqdn  = "oauth-openshift.apps.openshift-01.hashicorp.local"
-  }]
+  # Vault credential configurations
+  use_credentials      = false
 
-  services = [{
-    type             = "tcp"
-    port             = 443
-    use_existing_creds = false
-    use_vault_creds    = false
-  }]
-}
-
-# create another target for controller-aap.apps.openshift-01.hashicorp.local
-module "controller_target" {
-  source = "app.terraform.io/tfo-apj-demos/target/boundary"
-  version = "~> 2.0.1"
-
-  project_name           = "gcve_admins"
-  hostname_prefix        = "Controller Openshift Console"
-
-  hosts = [{
-    fqdn  = "controller-aap.apps.openshift-01.hashicorp.local"
-  }]
-
-  services = [{
-    type             = "tcp"
-    port             = 443
-    use_existing_creds = false
-    use_vault_creds    = false
-  }]
-}*/
-
-removed {
-  from = module.openshift_target
-
-  lifecycle {
-    destroy = false
-  }
-}
-
-removed {
-  from = module.oauth_target
-
-  lifecycle {
-    destroy = false
-  }
-}
-
-removed {
-  from = module.controller_target
-
-  lifecycle {
-    destroy = false
-  }
+  # Alias name for accessing the AAP Openshift Console
+  alias_name           = "oauth-openshift.apps.openshift-01.hashicorp.local"
 }
