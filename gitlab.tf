b81577e2221402a333f4eb1c05c87777ca6c2c08
Simon Lynch <simon.lynch@hashicorp.com>
2024-11-22 17:32:39 +1100

    Create GitLab OperatorGroup and Subscription manifests with updated namespace

---
+++ gitlab.tf
@@ -0,0 +1,29 @@
+locals {  
+  gitlab_operatorgroup = file("${path.module}/manifests/gitlab/gitlab-operatorgroup.yaml")
+  gitlab_subscription = file("${path.module}manifests/gitlab/gitlab-subscription.yaml")
+}
+
+resource "kubernetes_namespace" "gitlab" {
+  metadata {
+    name = "gitlab"
+  }
+
+    lifecycle {
+    
+    ignore_changes = [
+        metadata.0.annotations["openshift.io/sa.scc.mcs"],
+        metadata.0.annotations["openshift.io/sa.scc.supplemental-groups"],
+        metadata.0.annotations["openshift.io/sa.scc.uid-range"],
+    ]
+    }
+}
+
+
+resource "kubernetes_manifest" "gitlab_subscription" {
+  manifest = provider::kubernetes::manifest_decode(local.gitlab_subscription)
+}
+
+# Subscription resource
+resource "kubernetes_manifest" "gitlab_operatorgroup" {
+  manifest = provider::kubernetes::manifest_decode(local.gitlab_operatorgroup)
+}
