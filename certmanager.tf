73ad6c515559bd275d70ca8092d7d05cbac1a2c2
Simon Lynch <simon.lynch@hashicorp.com>
2024-12-11 19:58:48 +1100

    Add labels to certmanager namespace metadata

--- certmanager.tf
+++ certmanager.tf
@@ -14,6 +14,7 @@ resource "kubernetes_namespace" "certmanager" {
         metadata.0.annotations["openshift.io/sa.scc.mcs"],
         metadata.0.annotations["openshift.io/sa.scc.supplemental-groups"],
         metadata.0.annotations["openshift.io/sa.scc.uid-range"],
+        metadata.0.labels
     ]
     }
 }
