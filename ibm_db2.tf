c2a5648832de0ef6a652d223c597d2590aa2fce5
Simon Lynch <simon.lynch@hashicorp.com>
2025-03-10 12:34:18 +1100

    Uncomment DB2 Kubernetes manifests for catalog, operator group, and subscription

--- ibm_db2.tf
+++ ibm_db2.tf
@@ -20,17 +20,17 @@ resource "kubernetes_namespace" "db2" {
   
 }
 
-# resource "kubernetes_manifest" "db2_catalog" {
-#   manifest = provider::kubernetes::manifest_decode(local.ibm_catalog)
-# }
+resource "kubernetes_manifest" "db2_catalog" {
+  manifest = provider::kubernetes::manifest_decode(local.ibm_catalog)
+}
 
 
-# resource "kubernetes_manifest" "db2_operatorgroup" {
-#   depends_on = [kubernetes_namespace.db2]
-#   manifest   = provider::kubernetes::manifest_decode(local.db2_operatorgroup)
-# }
+resource "kubernetes_manifest" "db2_operatorgroup" {
+  depends_on = [kubernetes_namespace.db2]
+  manifest   = provider::kubernetes::manifest_decode(local.db2_operatorgroup)
+}
 
-# resource "kubernetes_manifest" "db2_subscription" {
-#   depends_on = [kubernetes_namespace.db2]
-#   manifest   = provider::kubernetes::manifest_decode(local.db2_subscription)
-# }
+resource "kubernetes_manifest" "db2_subscription" {
+  depends_on = [kubernetes_namespace.db2]
+  manifest   = provider::kubernetes::manifest_decode(local.db2_subscription)
+}
