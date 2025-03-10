7bb6b2a72bf89acc8f4f27162579c0e97154b5e6
Simon Lynch <simon.lynch@hashicorp.com>
2025-03-10 11:29:44 +1100

    Update ibm_db2.tf to reference ibm-catalog.yaml for consistency

--- ibm_db2.tf
+++ ibm_db2.tf
@@ -1,8 +1,8 @@
 locals {
-  db2_catalog        = file("${path.module}/manifests/ibm-db2/db2-catalog.yaml")
+  ibm_catalog        = file("${path.module}/manifests/ibm-db2/ibm-catalog.yaml")
 
 }
 
 resource "kubernetes_manifest" "db2_catalog" {
-  manifest = provider::kubernetes::manifest_decode(local.db2_catalog)
+  manifest = provider::kubernetes::manifest_decode(local.ibm_catalog)
 }
