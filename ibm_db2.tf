1918c9adeca31712cba9fe277c9299d1c7de420d
Simon Lynch <simon.lynch@hashicorp.com>
2025-03-10 12:07:10 +1100

    Add DB2 operator group and subscription manifests to ibm_db2.tf

--- ibm_db2.tf
+++ ibm_db2.tf
@@ -1,8 +1,27 @@
 locals {
   ibm_catalog        = file("${path.module}/manifests/ibm-db2/ibm-catalog.yaml")
+  db2_operatorgroup  = file("${path.module}/manifests/ibm-db2/db2-operatorgroup.yaml")
+  db2_subscription   = file("${path.module}/manifests/ibm-db2/db2-subscription.yaml")
 
 }
 
+resource "kubernetes_namespace" "db2u" {
+  metadata {
+    name = "db2u"
+  }
+}
+
 resource "kubernetes_manifest" "db2_catalog" {
+  manifest = provider::kubernetes::manifest_decode(local.ibm_catalog)
+}
+
+
+resource "kubernetes_manifest" "db2_operatorgroup" {
+  depends_on = [ kubeernetes_namespace.db2u ]
+  manifest = provider::kubernetes::manifest_decode(local.ibm_catalog)
+}
+
+resource "kubernetes_manifest" "db2_subscription" {
+  depends_on = [ kubeernetes_namespace.db2u ]
   manifest = provider::kubernetes::manifest_decode(local.ibm_catalog)
 }
